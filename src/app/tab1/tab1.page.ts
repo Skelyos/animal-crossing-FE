93d4d1242e6b27d53e8753c7d97ffb4156e0fc6b
Skelyos <pat123john123@gmail.com>
2022-09-07 20:28:30 +0100

    Created a page the is paginated & allows you to search for items by name

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() {}
+  public currentPageNumber = 1;
+  public totalItems: number;
+  public itemListData = [];
+  public pageLimit: number;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getAllItems();
+  }
+
+  getAllItems(newPage: number = 0) {
+    return new Promise<void>((resolve, reject) => {
+      const itemsUrl = `https://api.churchofiron.co.uk/items/all?page_number=${newPage}`;
+      this.http.get(itemsUrl).subscribe((res: any) => {
+        this.itemListData.push(...res.items);
+        this.totalItems = res.total;
+        this.pageLimit = Math.ceil(res.total / res.page_size);
+
+        resolve();
+      });
+    });
+  }
+
+  async loadMoreData(event) {
+    const newPage = this.currentPageNumber++;
+    await this.getAllItems(newPage);
+    event.target.complete();
+    if (this.currentPageNumber === this.pageLimit) {
+      event.target.disabled = true;
+    }
+  }
+
+  searchSpecificItem(searchValue: CustomEvent) {
+    if (!searchValue?.detail?.value) {
+      this.clearItems();
+      return;
+    }
+
+    const searchItemsUrl = `https://api.churchofiron.co.uk/items/search?search_text=${searchValue?.detail?.value}`;
+    this.http.get(searchItemsUrl).subscribe((res: any) => {
+      this.infiniteScroll.disabled = true;
+      this.currentPageNumber = 0;
+      this.itemListData = res;
+
+      console.log(res.length);
+    });
+  }
+
+  clearItems() {
+    this.infiniteScroll.disabled = false;
+    this.currentPageNumber = 1;
+    this.itemListData = [];
+    this.getAllItems();
+  }
 }
