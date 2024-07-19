834e7b24686c05654329ea7c8644e5b0dba71a48
skelyos <pat123john123@gmail.com>
2024-07-19 22:51:40 +0100

    Fixed weird iOS styling & also removed unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
+  {
+    path: 'item-search',
+    loadChildren: () => import('./item-search/item-search.module').then(m => m.ItemSearchModule)
+  },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: '/item-search',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs/item-search',
+    redirectTo: '/item-search',
+    pathMatch: 'full'
   }
 ];
 @NgModule({
